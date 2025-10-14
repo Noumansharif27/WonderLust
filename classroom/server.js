7c0b9ae3d8e7ef96d4120d2a4a65bf4f9b5ee993
noumansharif27 <noumansharifgul27.com>
2025-10-14 17:55:32 +0500

    working on flash

--- classroom/server.js
+++ classroom/server.js
@@ -8,13 +8,12 @@ app.use(flash());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(
-  session({
-    secret: "myDeepDarkSecret", // A secreat code which is best to be not read by human
-    resave: false, // forces the session to safe in the session store even if the session is not modified during the request, Default is true
-    saveUninitialized: true, // Forces the session to save that is uninitilized, new or not modified to be saved to the session store
-  })
-);
+const sessionOptions = {
+  secret: "myDeepDarkSecret", // A secreat code which is best to be not read by human
+  resave: false, // forces the session to safe in the session store even if the session is not modified during the request, Default is true
+  saveUninitialized: true, // Forces the session to save that is uninitilized, new or not modified to be saved to the session store
+};
+app.use(session(sessionOptions));
 
 // Using express-session to create a new variable for tracking the request quantity sent by the user.
 app.get("/test", (req, res) => {
@@ -32,6 +31,12 @@ app.listen(3000, () => {
 
 app.get("/register", (req, res) => {
   const { name = "anonymous" } = req.query;
-  console.log(name);
+  req.session.name = name;
+  req.flash("success", "user added successfully!");
   res.send(`Hello ${name}`);
+});
+
+app.get("/hello", (req, res) => {
+  console.log(flash("success"));
+  app.render("index.ejs", { name: req.session.name });
 });
