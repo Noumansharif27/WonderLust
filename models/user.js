ca584db29bb503d4a49ad5ceb4ff2cca6278c665
noumansharif27 <noumansharifgul27@gmail.com>
2025-10-17 18:50:28 +0500

    hm

--- models/user.js
+++ models/user.js
@@ -11,4 +11,4 @@ const userSchema = new Schema({
 
 User.plugin(passportLocalMongoose);
 
-module.exports = Model.mongoose(("user", userSchema)); // user model
+module.exports = Model.mongoose(("user", userSchema));
