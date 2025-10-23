ef78d0f4e06e4ee87ef1a76e637c625bc478423c
noumansharif27 <noumansharifgul27@gmail.com>
2025-10-23 14:15:18 +0500

    signup the user successfully

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const asyncWrap = require("../utils/asyncWrap.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
+
+router.post(
+  "/signup",
+  asyncWrap(async (req, res) => {
+    const { username, password, email } = req.body;
+
+    const newUser = new User({ username, email });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.flash("success", "Welcome to Wonderlust");
+    res.redirect("/listings");
+  })
+);
 
 module.exports = router;
