cc2133b608d678359f796f97080e30d2c9868cb2
noumansharif27 <noumansharifgul27@gmail.com>
2025-10-29 13:06:43 +0500

    introduced to claudinary and .env

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// requiring the .env file and using it once in production
+if (process.env_NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
